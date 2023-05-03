886dd8a573ca133f773c1de5a38fdf76c1ed8bb4
Kyle Henderson <ona@lunch.run>
2023-05-03 15:24:10 -0500

    PARSING!!!

--- sheet/src/lib.rs
+++ sheet/src/lib.rs
@@ -1,9 +1,38 @@
 extern crate google_sheets4 as sheets4;
 
+use serde::Deserialize;
+use serde_json::Value;
 use sheets4::Sheets;
 use sheets4::{oauth2, oauth2::ServiceAccountAuthenticator};
 
-pub async fn get_sheet(service_account_path: String, spreadsheet_id: String) {
+#[derive(Debug, Deserialize, Clone)]
+struct Meetup {
+    #[serde(rename = "Running Group")]
+    group: String,
+    #[serde(rename = "Day of the Week")]
+    day: String,
+    #[serde(rename = "Time")]
+    time: String,
+    #[serde(rename = "Description")]
+    description: String,
+    #[serde(rename = "Location")]
+    location: String,
+}
+
+#[derive(Debug)]
+struct Meetups {
+    monday: Vec<Meetup>,
+    tuesday: Vec<Meetup>,
+    wednesday: Vec<Meetup>,
+    thursday: Vec<Meetup>,
+    friday: Vec<Meetup>,
+    saturday: Vec<Meetup>,
+    sunday: Vec<Meetup>,
+}
+
+pub async fn create_sheets(
+    service_account_path: String,
+) -> Sheets<hyper_rustls::HttpsConnector<hyper::client::HttpConnector>> {
     let creds = oauth2::read_service_account_key(service_account_path)
         .await
         .expect("Can't read credential, an error occurred");
@@ -13,7 +42,7 @@ pub async fn get_sheet(service_account_path: String, spreadsheet_id: String) {
         .await
         .expect("There was an error, trying to build connection with authenticator");
 
-    let hub = Sheets::new(
+    Sheets::new(
         hyper::Client::builder().build(
             hyper_rustls::HttpsConnectorBuilder::new()
                 .with_native_roots()
@@ -23,43 +52,92 @@ pub async fn get_sheet(service_account_path: String, spreadsheet_id: String) {
                 .build(),
         ),
         sa,
-    );
+    )
+}
 
-    let result = hub.spreadsheets().get(&spreadsheet_id).doit().await;
+pub async fn get_sheet(
+    hub: Sheets<hyper_rustls::HttpsConnector<hyper::client::HttpConnector>>,
+    spreadsheet_id: &String,
+    sheet_name: &String,
+) -> Result<Vec<Vec<Value>>, String> {
+    let resp = hub
+        .spreadsheets()
+        .values_get(spreadsheet_id, sheet_name)
+        .doit()
+        .await;
 
-    match result {
+    match resp {
         Ok((_, response)) => {
-            let sheets = response.sheets.unwrap();
-
-            for sheet in sheets {
-                let properties = sheet.properties.unwrap();
-                let title = properties.title.unwrap();
-
-                // The lib does not like the / char
-                if title == "Races/Events" {
-                    continue;
+            let values = response.values;
+            match values {
+                Some(values) => {
+                    return Ok(values);
                 }
-
-                let values = hub
-                    .spreadsheets()
-                    .values_get(&spreadsheet_id, &title)
-                    .doit()
-                    .await;
-
-                match values {
-                    Ok((_, response)) => {
-                        println!("\nThe values: {:?}", response);
-                    }
-                    Err(e) => {
-                        println!("There was an error: {:?}", e);
-                    }
+                None => {
+                    return Err("No Values".to_string());
                 }
             }
         }
         Err(e) => {
-            println!("There was an error: {:?}", e);
+            let error = format!("There was an error: {:?}", e);
+            return Err(error);
         }
     }
+}
+
+async fn get_meetups(
+    hub: Sheets<hyper_rustls::HttpsConnector<hyper::client::HttpConnector>>,
+    spreadsheet_id: &String,
+) -> Option<Meetups> {
+    let values = get_sheet(hub, spreadsheet_id, &"Meetups".to_string()).await;
+    if let Err(e) = values {
+        println!("Error: {}", e);
+        return None;
+    }
+    let values = values.unwrap();
+
+    let csv = values
+        .iter()
+        .map(|row| {
+            let row = row
+                .iter()
+                .map(|cell| match cell {
+                    Value::String(s) => format!("\"{}\"", s),
+                    _ => "".to_string(),
+                })
+                .collect::<Vec<String>>()
+                .join(",");
+            row
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    println!("CSV: {}", csv);
+    let mut csv_reader = csv::Reader::from_reader(csv.as_bytes());
+    let mut meetups: Meetups = Meetups {
+        monday: Vec::new(),
+        tuesday: Vec::new(),
+        wednesday: Vec::new(),
+        thursday: Vec::new(),
+        friday: Vec::new(),
+        saturday: Vec::new(),
+        sunday: Vec::new(),
+    };
+    for result in csv_reader.deserialize() {
+        let meetup: Meetup = result.unwrap();
+
+        match meetup.day.clone() {
+            day if day.contains("Monday") => meetups.monday.push(meetup),
+            day if day.contains("Tuesday") => meetups.tuesday.push(meetup),
+            day if day.contains("Wednesday") => meetups.wednesday.push(meetup),
+            day if day.contains("Thursday") => meetups.thursday.push(meetup),
+            day if day.contains("Friday") => meetups.friday.push(meetup),
+            day if day.contains("Saturday") => meetups.saturday.push(meetup),
+            day if day.contains("Sunday") => meetups.sunday.push(meetup),
+            _ => (),
+        }
+    }
+    return Some(meetups);
 }
 
 #[cfg(test)]
@@ -71,8 +149,33 @@ mod tests {
         dotenv::dotenv().ok();
         let service_account_path = dotenv::var("SERVICE_ACCOUNT_PATH").unwrap();
         let spreadsheet_id = dotenv::var("SPREADSHEET_ID").unwrap();
+        let sheet = "Events".to_string();
 
-        get_sheet(service_account_path.to_string(), spreadsheet_id.to_string()).await;
+        let hub = create_sheets(service_account_path).await;
+        let values = get_sheet(hub, &spreadsheet_id, &sheet).await;
+
+        match values {
+            Ok(values) => {
+                println!("Values: {:?}", values);
+                assert!(values.len() > 0);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                assert!(false);
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_meetups() {
+        dotenv::dotenv().ok();
+        let service_account_path = dotenv::var("SERVICE_ACCOUNT_PATH").unwrap();
+        let spreadsheet_id = dotenv::var("SPREADSHEET_ID").unwrap();
+
+        let hub = create_sheets(service_account_path).await;
+        let meetups = get_meetups(hub, &spreadsheet_id).await;
+
+        print!("Meetups: {:?}", meetups);
     }
 
     // #[tokio::test]
